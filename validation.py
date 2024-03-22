df246316b71ddaf1ddda6e2f44b71d1e03cf30a3
j-fangwu <johnfangwu@gmail.com>
2024-03-22 16:12:09 -0400

    Added a preliminary validation code

---
+++ validation.py
@@ -0,0 +1,42 @@
+import csv
+
+def validate_id(class_number, id_val, filename='UFIDProjectSampleDatabase.csv'):
+    with open(filename, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            if (barcode_val == row[0] or barcode_val == row[1]):
+                class_numbers = row[4].split()
+                if class_number in class_numbers:
+                    return True
+    return False
+
+if __name__ == '__main__':
+
+    filename = "ufid_barcodes.csv"
+
+    barcodes = []
+
+    field = ['Barcode Value']
+
+    class_number = input("Enter the class number: ")
+
+    scan = True
+
+    while scan:
+        barcode_val = input("Scan a barcode right now: ")
+        if (barcode_val == ''):
+            scan = False
+        else:
+            if validate_id(class_number, barcode_val):
+                print("Valid ID")
+                barcodes.append([barcode_val])
+            else:
+                print("Invalid ID. Please try again.")
+        
+    
+    with open(filename, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(field)
+        csvwriter.writerows(barcodes)
+
+    print("Barcodes Stored")
