2d00d6b208b918525ae3f89d9a0393c780d52494
Brianna Rodriguez <brianna.rodriguez2240@gmail.com>
2024-03-29 11:16:08 -0400

    created basic visual ui for ufid id reader

---
+++ gui.py
@@ -0,0 +1,170 @@
+# import customtkinter
+# from PIL import Image
+
+# customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+# customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+
+# class App(customtkinter.CTk):
+#     def __init__(self):
+#         super().__init__()
+
+#         # configure window
+#         self.title("UFID READER")
+#         self.geometry(f"{1100}x{580}") 
+
+
+#         self.sidebar = SideBar(self,header_name="UFID Attendance")
+#         self.sidebar.grid(row=0, column=0, padx=10, pady=10)
+
+
+#         self.home_frame = ScanFrame(self)
+#         self.home_frame.grid(row=0, column=1, padx=5, pady=5)
+
+#         #frames
+#         self.frames = {}
+
+#     def sidebar_button_event(self):
+#         print("Button pressed")
+
+# class ScanFrame(customtkinter.CTkFrame):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+
+#         #home_frame = customtkinter.CTkFrame(self, bg_color="green")
+#         self.configure(width=810, height=560)
+
+#         label = customtkinter.CTkLabel(self, text="Scan UFID", font=("Roboto", 50))
+#         label.grid(pady=5, padx=10)
+#         label2 = customtkinter.CTkLabel(self, text="or", font=("Roboto", 50))
+#         label2.grid(pady=5, padx=10)
+#         label3 = customtkinter.CTkLabel(self, text="Tap Below", font=("Roboto", 50))
+#         label3.grid(pady=5, padx=10)
+
+#         img = customtkinter.CTkImage(light_image=Image.open("images/arrow.png"), dark_image=Image.open("images/arrow.png"), size=(200,200))
+#         img_label = customtkinter.CTkLabel(self, text='', image=img)
+#         img_label.grid(pady = 40)
+
+# class SideBar(customtkinter.CTkFrame):
+#     def __init__(self, *args, header_name="Scan Frame", **kwargs):
+#         super().__init__(*args, **kwargs)
+
+#         self.header_name = header_name
+#         self.header = customtkinter.CTkLabel(self, text=self.header_name)
+#         self.header.grid(padx=5, pady=5)
+
+#         side_frame = customtkinter.CTkFrame(self, bg_color="green")
+#         side_frame.configure(width=250, height=520)
+#         side_frame.grid(pady=2, padx=2)
+
+
+            
+
+
+# if __name__ == "__main__":
+#     app = App()
+#     app.mainloop()
+
+import customtkinter
+import os
+from PIL import Image
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("image_example.py")
+        self.geometry("1000x600")
+
+        # set grid layout 1x2
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        # create navigation frame
+        self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
+        self.navigation_frame.grid(row=0, column=0, sticky="nsew")
+        self.navigation_frame.grid_rowconfigure(4, weight=1)
+
+        self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="UFID ATTENDANCE CHECK",
+                                                             compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
+        self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
+
+        self.home_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Scan ID",
+                                                   fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                    anchor="w", command=self.home_button_event)
+        self.home_button.grid(row=1, column=0, sticky="ew")
+
+        self.frame_2_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Manual Search",
+                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                       anchor="w", command=self.frame_2_button_event)
+        self.frame_2_button.grid(row=2, column=0, sticky="ew")
+
+        self.appearance_mode_menu = customtkinter.CTkOptionMenu(self.navigation_frame, values=["Light", "Dark", "System"],
+                                                                command=self.change_appearance_mode_event)
+        self.appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
+
+        # create home frame
+        self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.home_frame.grid_columnconfigure(0, weight=1)
+
+        self.home_frame_large_image_label = customtkinter.CTkLabel(self.home_frame, text="")
+        self.home_frame_large_image_label.grid(row=0, column=0, padx=20, pady=10)
+
+        label = customtkinter.CTkLabel(self.home_frame, text="Scan UFID", font=("Roboto", 50))
+        label.grid(pady=5, padx=10)
+        label2 = customtkinter.CTkLabel(self.home_frame, text="or", font=("Roboto", 50))
+        label2.grid(pady=5, padx=10)
+        label3 = customtkinter.CTkLabel(self.home_frame, text="Tap Below", font=("Roboto", 50))
+        label3.grid(pady=5, padx=10)
+
+        img = customtkinter.CTkImage(light_image=Image.open("images/arrow.png"), dark_image=Image.open("images/arrow.png"), size=(200,200))
+        img_label = customtkinter.CTkLabel(self.home_frame, text='', image=img)
+        img_label.grid(pady = 40)
+
+        # create second frame
+        self.second_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        manual_label = customtkinter.CTkLabel(self.second_frame, text="Type UFID", font=("Roboto", 50))
+        manual_label.grid(pady=5, padx=10)
+        manual_label.place(anchor="c",relx=0.5, rely=0.15)
+        manual_label2 = customtkinter.CTkLabel(self.second_frame, text="Below", font=("Roboto", 50))
+        manual_label2.grid(pady=5, padx=10)
+        manual_label2.place(anchor="c",relx=0.5, rely=0.30)
+        input_box = customtkinter.CTkEntry(self.second_frame, placeholder_text="Type in UFID", height=50, width=200, justify='center', corner_radius=15, state="normal")
+        input_box.grid(pady=20)
+        input_box.place(anchor="c",relx=0.5, rely=0.5)
+        search_button = customtkinter.CTkButton(self.second_frame, text="Search", width=150, height=40, corner_radius=10, font=("Roboto", 15))
+        search_button.grid(padx=5, pady=10)
+        search_button.place(anchor="c", relx=0.5, rely=0.65)
+
+        # select default frame
+        self.select_frame_by_name("home")
+
+    def select_frame_by_name(self, name):
+        # set button color for selected button
+        self.home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
+        self.frame_2_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
+
+        # show selected frame
+        if name == "home":
+            self.home_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.home_frame.grid_forget()
+        if name == "frame_2":
+            self.second_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.second_frame.grid_forget()
+
+    def home_button_event(self):
+        self.select_frame_by_name("home")
+
+    def frame_2_button_event(self):
+        self.select_frame_by_name("frame_2")
+
+    def change_appearance_mode_event(self, new_appearance_mode):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
---
+++ landing.py
@@ -0,0 +1,28 @@
+import customtkinter
+from PIL import Image
+
+customtkinter.set_appearance_mode("light")
+customtkinter.set_default_color_theme("blue")
+
+root = customtkinter.CTk()
+root.geometry("500x600")
+
+landing_frame = customtkinter.CTkFrame(master=root)
+landing_frame.pack(pady=2, padx=2, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=landing_frame, text="Scan UFID", font=("Roboto", 50))
+label.pack(pady=5, padx=10)
+label2 = customtkinter.CTkLabel(master=landing_frame, text="or", font=("Roboto", 50))
+label2.pack(pady=5, padx=10)
+label3 = customtkinter.CTkLabel(master=landing_frame, text="Tap Below", font=("Roboto", 50))
+label3.pack(pady=5, padx=10)
+
+img = customtkinter.CTkImage(light_image=Image.open("images/arrow.png"), dark_image=Image.open("images/arrow.png"), size=(200,200))
+img_label = customtkinter.CTkLabel(master=landing_frame, text='', image=img)
+img_label.pack(pady = 40)
+
+search_button = customtkinter.CTkButton(master=landing_frame, text="Manual Search", width=75, height=50, corner_radius=10,fg_color="blue")
+search_button.place(x=350,y=500)
+
+
+root.mainloop()
