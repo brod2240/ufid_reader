0b053531455b7d389b69ef87276aed8babdb3c25
AaronSong7 <Aaronjsong7@gmail.com>
2024-11-10 18:40:54 -0500

    Update validation.py

--- UFIDReader/Packages/Validation/validation.py
+++ UFIDReader/Packages/Validation/validation.py
@@ -8,7 +8,7 @@ def web_api_get_request(page, params):
 
     return response    
 
-def validate(serial_num, card_iso=None, card_ufid=None, examMode):
+def validate(mode, serial_num, card_iso=None, card_ufid=None):
     params = {
         "serial_num": serial_num,
         "iso": card_iso,
@@ -62,6 +62,8 @@ def validate(serial_num, card_iso=None, card_ufid=None, examMode):
     now = datetime.now()
     #now = datetime(2024, 9, 19, 11, 0, 0)
 
+    date = now.strftime("%m/%d/%Y")
+
     day = now.weekday()
 
     #actual_now = datetime.now() #for prof website post request
@@ -93,16 +95,22 @@ def validate(serial_num, card_iso=None, card_ufid=None, examMode):
             }
     print(day)
     print(room)
+    print()
         
-    params = {
+    params1 = {
         "day": day,
         "roomCode": room
     }
 
-    if examMode:
-        results = (web_api_get_request(page='exams', params=params)).json()
+    params2 = {
+        "serial_num": serial_num,
+        "date": date
+    }
+
+    if mode == 1:
+        results = (web_api_get_request(page='exams', params=params2)).json()
     else:
-        results = (web_api_get_request(page='courses', params=params)).json()
+        results = (web_api_get_request(page='courses', params=params1)).json()
     
     # What if no class in room on day??
     # Tested it with Saturday seems fine just registers as no match -3
@@ -110,6 +118,7 @@ def validate(serial_num, card_iso=None, card_ufid=None, examMode):
 
     print(results)
     #print(results)
+    print()
 
     courses = []
 
@@ -118,8 +127,13 @@ def validate(serial_num, card_iso=None, card_ufid=None, examMode):
     for result in results:
         #start = datetime.strptime(result[5], '%I:%M %p') - timedelta(minutes=15)
         #end = datetime.strptime(result[6], '%I:%M %p') + timedelta(minutes=15) #possible change
-        start = datetime.strptime(result[6], '%I:%M %p').replace(year=now.year, month=now.month, day=now.day) - timedelta(minutes=15)
-        end = datetime.strptime(result[7], '%I:%M %p').replace(year=now.year, month=now.month, day=now.day) + timedelta(minutes=15)
+        if mode == 1:
+            start = datetime.strptime(result[6], '%I:%M %p').replace(year=now.year, month=now.month, day=now.day) #- timedelta(minutes=15)
+            end = datetime.strptime(result[7], '%I:%M %p').replace(year=now.year, month=now.month, day=now.day) #+ timedelta(minutes=15)
+        else:
+            start = datetime.strptime(result[6], '%I:%M %p').replace(year=now.year, month=now.month, day=now.day) - timedelta(minutes=15)
+            end = datetime.strptime(result[7], '%I:%M %p').replace(year=now.year, month=now.month, day=now.day) + timedelta(minutes=15)
+    
         #print(start)
         #print(current_time)
         #print(end)
@@ -127,52 +141,95 @@ def validate(serial_num, card_iso=None, card_ufid=None, examMode):
             courses.append(result)
 
 
-    #print(courses)
+    print(courses)
 
-    for course in courses:
-        course_sec_num = course[2]
-        for student_sec_num in student_sec_nums:
-            if course_sec_num == student_sec_num:
-                params = {
-                    'serial_num': serial_num, 
-                    'ufid': ufid,
-                    'iso': iso, 
-                    'first_name': first_name, 
-                    'last_name': last_name,
-                    'course': course[0],
-                    'class': course[2],
-                    'instructor': course[3],
-                    'room_num': course[8],
-                    'time':  now.strftime("%m/%d/%Y %I:%M:%S %p")
-                }
+    if mode == 1:
+        for course in courses:
+            course_sec_nums = course[3].split(', ')
+            for student_sec_num in student_sec_nums:
+                if student_sec_num in course_sec_nums:
+                    params = {
+                        'serial_num': serial_num, 
+                        'ufid': ufid,
+                        'iso': iso, 
+                        'first_name': first_name, 
+                        'last_name': last_name,
+                        'course': course[0],
+                        'class': student_sec_num,
+                        'instructor': course[2],
+                        'room_num': course[4],
+                        'time':  now.strftime("%m/%d/%Y %I:%M:%S %p")
+                    }
 
-                checkin_site_params = {
-                    'serial_num': serial_num, 
-                    'ufid': ufid,
-                    'iso': iso, 
-                    'first_name': first_name, 
-                    'last_name': last_name,
-                    'course': course[0],
-                    'class': course[2],
-                    'instructor': course[3],
-                    'room_num': course[8],
-                    'time': now.strftime("%Y-%m-%d %H:%M:%S")
-                }
+                    checkin_site_params = {
+                        'serial_num': serial_num, 
+                        'ufid': ufid,
+                        'iso': iso, 
+                        'first_name': first_name, 
+                        'last_name': last_name,
+                        'course': course[0],
+                        'class': student_sec_num,
+                        'instructor': course[2],
+                        'room_num': course[4],
+                        'time': now.strftime("%Y-%m-%d %H:%M:%S")
+                    }
 
-                #print(student_sec_num)
-                #do post request to timesheet
-                url = "https://gatorufid.pythonanywhere.com/timesheet"
-                response = requests.post(url, params=params)
+                    #print(student_sec_num)
+                    #do post request to timesheet
+                    url = "https://gatorufid.pythonanywhere.com/timesheet"
+                    response = requests.post(url, params=params)
 
-                checkin_web_url = "https://brirod2240.pythonanywhere.com/api/add_timesheet"
-                site_response = requests.post(checkin_web_url, json=checkin_site_params)
-                #print(site_response)
-                #print(site_response.text)
+                    checkin_web_url = "https://brirod2240.pythonanywhere.com/api/add_timesheet"
+                    site_response = requests.post(checkin_web_url, json=checkin_site_params)
+                    #print(site_response)
+                    #print(site_response.text)
 
-                is_valid = 0
+                    is_valid = 0
+    else:
+        for course in courses:
+            course_sec_num = course[2]
+            for student_sec_num in student_sec_nums:
+                if course_sec_num == student_sec_num:
+                    params = {
+                        'serial_num': serial_num, 
+                        'ufid': ufid,
+                        'iso': iso, 
+                        'first_name': first_name, 
+                        'last_name': last_name,
+                        'course': course[0],
+                        'class': course[2],
+                        'instructor': course[3],
+                        'room_num': course[8],
+                        'time':  now.strftime("%m/%d/%Y %I:%M:%S %p")
+                    }
+
+                    checkin_site_params = {
+                        'serial_num': serial_num, 
+                        'ufid': ufid,
+                        'iso': iso, 
+                        'first_name': first_name, 
+                        'last_name': last_name,
+                        'course': course[0],
+                        'class': course[2],
+                        'instructor': course[3],
+                        'room_num': course[8],
+                        'time': now.strftime("%Y-%m-%d %H:%M:%S")
+                    }
+
+                    #print(student_sec_num)
+                    #do post request to timesheet
+                    url = "https://gatorufid.pythonanywhere.com/timesheet"
+                    response = requests.post(url, params=params)
+
+                    checkin_web_url = "https://brirod2240.pythonanywhere.com/api/add_timesheet"
+                    site_response = requests.post(checkin_web_url, json=checkin_site_params)
+                    #print(site_response)
+                    #print(site_response.text)
+
+                    is_valid = 0
 
 
-    #fetch_courses()
+        #fetch_courses()
 
     validation = {
         "UFID": ufid,
@@ -184,7 +241,9 @@ def validate(serial_num, card_iso=None, card_ufid=None, examMode):
     return validation
 
 #fetch_courses(course_code="CHM6586")
-#valid = validate("10000000d340eb60", card_ufid="20000000")
+#valid = validate("10000000d340eb60", card_ufid="20000000", examMode="0")
 #print(valid)
-# valid = validate("10000000d340eb60", card_ufid="77211373")
-# print(valid)
+
+# THIS ONE WORKS
+#valid = validate(1, "10000000d340eb60", card_ufid="77211373")
+#print(valid)
